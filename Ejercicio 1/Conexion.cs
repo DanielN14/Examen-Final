3dbaa5f419b9fc2ca5f80df31cbd545d37582722
DanielN <jdnp2001@gmail.com>
2021-04-25 17:16:24 -0600

    Examen Final

---
+++ Ejercicio 1/Conexion.cs
@@ -0,0 +1,25 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace Ejercicio_1
+{
+    public class Conexion
+    {
+        string connectionString = @"Data Source=LAPTOP-JQD2SI9A\SQLEXPRESS;Initial Catalog=Ejercicio 1;Integrated Security=True";
+
+        public void Execute(string comando)
+        {
+            using (SqlConnection cnn = new SqlConnection(connectionString))
+            {
+                cnn.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = comando;
+                cmd.CommandType = CommandType.Text;
+                cmd.Connection = cnn;
+
+                
+                int i = cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
---
+++ Ejercicio 2/Conexion.cs
@@ -0,0 +1,25 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace Ejercicio_2
+{
+    public class Conexion
+    {
+        public string connetionString = @"Data Source=LAPTOP-JQD2SI9A\SQLEXPRESS;Initial Catalog=Ejercicio 1;Integrated Security=True";
+
+        public void Execute(string comando)
+        {
+            using (SqlConnection cnn = new SqlConnection(connetionString))
+            {
+                cnn.Open();
+
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = comando;
+                cmd.CommandType = CommandType.Text;
+                cmd.Connection = cnn;
+
+                int i = cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
